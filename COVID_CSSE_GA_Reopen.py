647a3b5a6c8d751db68f79b26d8d25ef35c9d8ed
ZanChaudhry <53440320+ZanChaudhry@users.noreply.github.com>
2020-04-23 17:00:01 -0400

    Update COVID_CSSE_GA_Reopen.py

--- COVID_CSSE_GA_Reopen.py
+++ COVID_CSSE_GA_Reopen.py
@@ -169,7 +169,7 @@ plt.plot(tSpaceT, ypoints, label="Cumulative Reported Cases (I+R)")
 plt.legend(loc='best')
 axes.set_xlabel('Time since '+StartDate+' (Days)')
 axes.set_ylabel('People')
-axes.set_title('COVID19 Model for US (SEIR, RK4)')
+axes.set_title('COVID19 Model for Georgia (SEIR, RK4)')
 plt.savefig('CurvesForCOVID19_GA.png')
 
 # =============================================================================
